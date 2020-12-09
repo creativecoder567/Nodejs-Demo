c1782db2b325de5a8e24c933d2c067beb16115fd
vinoth <vinoth.m@esecforte.com>
2020-12-09 21:19:09 +0530

    added ejs mpl

--- app.js
+++ app.js
@@ -19,8 +19,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// view engine setup
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', require('./src/routes/ui_route'));
 
 app.use('/api', require('./src/routes/api_route'));
 
