b9c9454a31b74676a0a33407c4f3bde91cc5a68d
vinoth <vinoth.m@esecforte.com>
2020-12-19 20:22:35 +0530

    added api

--- src/service/api_service.js
+++ src/service/api_service.js
@@ -2,6 +2,7 @@ const db = require('../model/index');
 const service = db['service'];
 const serviceCategory = db['service_category'];
 const banner = db['banner'];
+const contact_us = db['contact_us'];
 
 
 exports.serviceCreate = async (data) => {
@@ -32,6 +33,18 @@ exports.serviceCategoryCreate = async (data) => {
 exports.bannerCreate = async (data) => {
     try {
         var r = await banner.create(data, {
+            returning: true, individualHooks: true
+        });
+        return r;
+    } catch (e) {
+        console.log("eppp---", e);
+        throw e;
+    }
+};
+
+exports.addContactMessage = async (data) => {
+    try {
+        var r = await contact_us.create(data, {
             returning: true, individualHooks: true
         });
         return r;
