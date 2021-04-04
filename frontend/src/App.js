d1bd5d7735efc4fa4b6a0314b8f00f4373e3398c
sarath <sarathkfs@gmail.com>
2021-04-04 12:45:38 +0530

    product and rating component is added. react-router-dom is installed.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,23 @@
 import data from "./data";
-
+import Product from "./components/Product"
 function App() {
   return (
 <div className="grid-container">
       <header className="row">
         <div>
-          <a className="brand" href="index.html">amazona</a>
+          <a className="brand" href="">Gudiyatham Services</a>
         </div>
         <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
         </div>
       </header>
       <main>
         <div>
           <div className="row center">
             {
-              data.products.map(product=>(
-                <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                  <img className="medium" src={product.image} alt={product.name}/>
-                </a>
-                <div className="card-body">
-                <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span> <i className="fa fa-star"></i> </span>
-                    <span> <i className="fa fa-star"></i> </span>
-                    <span> <i className="fa fa-star"></i> </span>
-                    <span> <i className="fa fa-star"></i> </span>
-                    <span> <i className="fa fa-star"></i> </span>
-                  </div>
-                  <div className="price">${product.price}</div>
-                </div>
-              </div>
-
+              data.products.map((product)=>( 
+               <Product key={product._id} product={product}></Product>
               ))
             }
            
